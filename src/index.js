2485a8a94f1a1f49abae01f5c8edbc9cabf3c91b
Igor Shatrov <ihar.shatrov@gmail.com>
2020-10-08 08:31:12 +0300

    feat: add all functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    console.log(n + typeof(n))
+    let finish = ''
+    let arr = Math.abs(n).toString().split('')
+    console.log(arr);
+    for (let i = arr.length - 1; i >= 0; i--) {
+        finish += arr[i]
+    }
+    return parseInt(finish)
 }
